d7be255ada848a84326cf4fe1c374b5f8fc58f96
Ben Woodcroft <donttrustben@gmail.com>
2016-12-12 21:11:48 +1000

    gnu: Add r-bit64.
    * gnu/packages/statistics.scm (r-bit64): New variable.

--- gnu/packages/statistics.scm
+++ gnu/packages/statistics.scm
@@ -228,6 +228,32 @@ this means factor 32 less RAM and ~ factor 32 more speed on boolean
 operations.")
     (license license:gpl2)))
 
+(define-public r-bit64
+  (package
+    (name "r-bit64")
+    (version "0.9-5")
+    (source
+     (origin
+       (method url-fetch)
+       (uri (cran-uri "bit64" version))
+       (sha256
+        (base32
+         "0fz5m3fhvxgwjl76maag7yn0zdw24rx34gy6v77378fajag9yllg"))))
+    (build-system r-build-system)
+    (propagated-inputs
+     `(("r-bit" ,r-bit)))
+    (home-page "http://ff.r-forge.r-project.org/")
+    (synopsis "An S3 class for vectors of 64 bit Integers")
+    (description
+     "The bit64 package provides serializable S3 atomic 64 bit (signed)
+integers that can be used in vectors, matrices, arrays and data.frames.
+Methods are available for coercion from and to logicals, integers, doubles,
+characters and factors as well as many elementwise and summary functions.
+Many fast algorithmic operations such as 'match' and 'order' support
+interactive data exploration and manipulation and optionally leverage
+caching.")
+    (license license:gpl2)))
+
 (define-public r-colorspace
   (package
     (name "r-colorspace")
